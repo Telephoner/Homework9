462d5ff8fb47364edc07f1088b2bd340b303fdd5
Telephoner <yougotacall@gmail.com>
2022-08-28 15:26:29 +0500

    done homework

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         Author witcherAuthor = new Author("Анджей", "Сапковский");
+        Author bookThiefAuthor = new Author("Маркус", "Зусак");
         Book witcher = new Book("Ведьмак: Последнее желание", witcherAuthor.getName() + " " + witcherAuthor.getSurname(), 1993);
+        Book bookThief = new Book("Книжный вор", bookThiefAuthor.getName() + " " + bookThiefAuthor.getSurname(), 2005);
 
-        System.out.println(witcher.getTitle()+" "+witcher.getReleaseYear()+ " " + witcher.getAuthor());
-
-
+        System.out.println(witcher.getTitle() + " " + witcher.getReleaseYear() + " " + witcher.getAuthor());
+        System.out.println(bookThief.getTitle() + " " + bookThief.getReleaseYear() + " " + bookThief.getAuthor());
+        witcher.setReleaseYear(2017);
+        System.out.println("witcher.getReleaseYear() = " + witcher.getReleaseYear());
     }
 }
